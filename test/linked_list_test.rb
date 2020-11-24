57b7e5c4324472ebd715021c9a7bb97e58b0bc28
Will Dunlap <dunlapww@gmail.com>
2020-11-23 19:57:46 -0700

    Add test for includes?

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -95,5 +95,18 @@ class LinkedListTest < Minitest::Test
     assert_equal "shu", list.pop
     assert_equal "deep woo shi", list.to_string
   end
+  
+  def test_it_can_includes?
+    list = LinkedList.new
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+    assert_equal "deep woo shi shu blop", list.to_string
+    assert list.includes?("deep")
+    assert list.includes?("shu")
+    refute list.includes?("dep")
+  end
 
 end
