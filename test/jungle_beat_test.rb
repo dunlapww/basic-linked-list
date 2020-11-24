ad8408cdb6cadf5523e68bbcb3c52110f07ab932
Will Dunlap <dunlapww@gmail.com>
2020-11-23 20:29:41 -0700

    Add test for jungle beat append and count

--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -7,4 +7,19 @@ class JungleBeatTest < Minitest::Test
     jb = JungleBeat.new
     assert_instance_of JungleBeat, jb
   end
+
+  def test_it_has_details
+    jb = JungleBeat.new
+    assert_instance_of LinkedList, jb.list
+    assert_nil jb.list.head
+  end
+  
+  def test_it_can_append_and_count
+    jb = JungleBeat.new
+    assert_equal "deep doo ditt", jb.append("deep doo ditt")
+    assert_equal "deep", jb.list.head.data
+    assert_equal "doo", jb.list.head.next_node.data
+    assert_equal "woo hoo shu", jb.append("woo hoo shu")
+    assert_equal 6, jb.count
+  end
 end
